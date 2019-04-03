53e877ec942ba1649538c0985647eefc389ee48b
Yoo In Keun <keun0390@naver.com>
2019-04-03 15:49:14 +0900

    부트스트랩 적용 및 기록 확인 페이지 추가
    Bootstrap Template and Record Page Addition

--- wordcount/models.py
+++ wordcount/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+from django.utils import timezone
+
+class Record(models.Model):
+
+    text = models.TextField()
+    word_all_count = models.CharField(max_length=100, default = 'None')
+    word_count = models.CharField(max_length=200, default = 'None')
+    created_at = models.DateTimeField(default = timezone.now)
+
+    def __str__(self):
+
+        return self.text
